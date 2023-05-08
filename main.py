f4b9c6f1b893ade259126e23215832b10289975e
tincho <mjvallone@gmail.com>
2023-05-08 13:38:57 -0300

    fixing some minor errors

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import streamlit as st
 from tempfile import NamedTemporaryFile
 import whisper
 import os
-# from tkinter import filedialog
-
 
 model = whisper.load_model("base")
 language = st.selectbox("Select Language", ["English", "Spanish"], index=1)
 
 
-def save_text_as_file(text, audio_filename):
-    save_directory = st.text_input("Save Directory", "/home/tincho/Documentos")
-    save_filename = st.text_input("Save Filename", audio_filename.replace(".mp3", ".txt").strip())
-    save_location = os.path.join(save_directory, save_filename)
-    #FIXME be able to save it to where you want, not fixed location
+def get_download_path():
+    """Returns the default downloads path for Linux or Windows"""
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        try:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+                location = winreg.QueryValueEx(key, downloads_guid)[0]
+            return location
+        except WindowsError:
+            pass
 
-    # save_location = st.text_input("Save Location", audio_filename.replace(".mp3", ".txt").strip())
-    # if st.button("Choose Directory"):
-    #     import pdb; pdb.set_trace()
-    #     root = st._root
-    #     root.withdraw()
-    #     folder_selected = filedialog.askdirectory()
-    #     save_location = os.path.join(folder_selected, os.path.basename(save_location))
+    # Fallback for Linux or when Windows registry key is not found
+    possible_dirs = ["Downloads", "Descargas"]
+    home_dir = os.path.expanduser('~')
+    for dir_name in possible_dirs:
+        dir_path = os.path.join(home_dir, dir_name)
+        if os.path.exists(dir_path):
+            return dir_path
+
+    return home_dir
+
+
+def save_text_as_file(text, audio_filename):
+    default_dir = get_download_path()
+    save_location = os.path.join(default_dir, audio_filename.replace(".mp3", ".txt"))
+    st.write(f"save_location: {save_location}")
 
     def save_file():
         location_label = "Saving text to" if language == "English" else "Guardando texto en"
@@ -50,14 +63,14 @@ def main():
 
     if audio_file is not None:
         transcribe_button = st.button(transcribe_button_label)
-        if  transcribe_button:
-            # Create a temporary file to save the uploaded MP3
-            with NamedTemporaryFile(suffix=".mp3", delete=False) as temp_file:
-                temp_file.write(audio_file.read())
-                temp_file.flush()
+        # Create a temporary file to save the uploaded MP3
+        with NamedTemporaryFile(suffix=".mp3", delete=False) as temp_file:
+            temp_file.write(audio_file.read())
+            temp_file.flush()
 
-                # Play the uploaded MP3 using playsound library
-                st.audio(temp_file.name, format='audio/mp3', start_time=0)
+            # Play the uploaded MP3 using playsound library
+            st.audio(temp_file.name, format='audio/mp3', start_time=0)
+            if  transcribe_button:
                 transcribe_spinner_label = "Transcribing audio..." if language == "English" else "Transcribiendo audio..."
                 with st.spinner(transcribe_spinner_label):
                     # Transcribe the uploaded MP3
